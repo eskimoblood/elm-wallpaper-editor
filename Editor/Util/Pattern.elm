9c7dd2b8027fde2cf3cc5b067593b787e447a343
Andreas Köberle <koeberle@gmail.com>
2016-01-22 16:58:39 +0100

    add noisy colors

--- Editor/Util/Pattern.elm
+++ Editor/Util/Pattern.elm
@@ -5,6 +5,18 @@ import Editor.Util.TileSize exposing (..)
 import Editor.Types exposing (Tile, Point, MultiLine, Bezier)
 import Editor.Model exposing (Model)
 import WallpaperGroup.Pattern as Pattern
+import Array
+
+
+getColor : List String -> Float -> String
+getColor colors noise =
+  let
+    color = Array.fromList colors
+      |> Array.get (floor ((toFloat (List.length colors) * (abs noise))))
+  in
+    case color of
+      Just cl -> "#" ++ cl
+      Nothing -> "grey"
 
 
 scalePoint : {a | width: Float, height: Float, groupType: String} -> Point -> Point
@@ -28,22 +40,23 @@ randomPoint p noise noiseDesctruction =
     {x= p.x + (cos angle) * rad, y= p.y + (sin angle) * rad}
 
 
-calcPath : Int -> (Float, MultiLine) -> Bezier
-calcPath noiseDesctruction (noise, line)  =
+calcPath : Int -> List String -> (Float, MultiLine) -> Bezier
+calcPath noiseDesctruction colors (noise, line)  =
   let
     p1 = Maybe.withDefault {x=0,y=0} (List.head line)
     p2 = Maybe.withDefault {x=0,y=0} (List.reverse line |> List.head)
     c1 = randomPoint p1 -noise noiseDesctruction
     c2 = randomPoint p2 noise noiseDesctruction
+    color = getColor colors noise
   in
-    {p1= p1, p2=p2, c1=c1, c2=c2}
+    {p1= p1, p2=p2, c1=c1, c2=c2, color= color, opacity= 1 - (abs noise)}
 
-calcTile : Int -> List (Float, MultiLine) -> List Bezier
-calcTile noiseDesctruction tile =
+calcTile : Int -> List String -> List (Float, MultiLine) -> List Bezier
+calcTile noiseDesctruction colors tile =
   let
-    lines = List.filter (\ (s,_) -> s > 0) tile
+    lines = List.filter (\ (s,_) -> (abs s) > 0.2) tile
   in
-    List.map (calcPath noiseDesctruction) lines
+    List.map (calcPath noiseDesctruction colors) lines
 
 
 updatePatternInModel : Model -> Model
@@ -59,6 +72,8 @@ updatePatternInModel  model =
     (noise, seed) =  (noise3d model maxZ)
     noisyGroups = (List.map2 (List.map2 (,)) noise groups)
     noiseDesctruction = patternState.noiseDesctruction
-    pattern = List.map (calcTile noiseDesctruction) noisyGroups
+    colors = model.colorState.selectedPalette
+    pattern = List.map (calcTile noiseDesctruction colors) noisyGroups
+
   in
     {model | patternState = {patternState | pattern = pattern}, seed = seed}
